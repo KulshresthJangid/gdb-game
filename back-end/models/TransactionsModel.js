afce5f1600531c68cfdf7512c74e12c82eb27f38
Kulshresth Jangid <kulshresthjangid@gmail.com>
2024-10-12 02:07:36 +0530

    added transaction model clas

---
+++ back-end/models/TransactionsModel.js
@@ -0,0 +1,9 @@
+const BaseController = require("../controllers/BaseController");
+
+class TransactionsModel extends BaseController {
+    constructor() {
+        super();
+    }
+}
+
+module.exports = new TransactionsModel();
