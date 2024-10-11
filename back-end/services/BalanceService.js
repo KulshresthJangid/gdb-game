c2c4a722c2de3a272ec8ed3a061b3f00580ff3ae
Kulshresth Jangid <kulshresthjangid@gmail.com>
2024-10-12 01:28:56 +0530

    Fix refactoring

--- back-end/services/BalanceService.js
+++ back-end/services/BalanceService.js
@@ -9,12 +9,11 @@ class BalanceService {
     async initiateRuleForNewUser(user) {
         // add bonus of 100 rs.
         let initialBonus = process.env.INITIAL_USER_BONUS;
-        console.log('initial bonus', initialBonus);
         if(initialBonus) {
             user.balance = parseInt(initialBonus);
             console.log('user after', user);
             await this.userDB.save(user);
-        }    
+        }
     }
 
     async addBalanceToUser(userId, balance) {
